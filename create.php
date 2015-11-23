79d2cf9a68f31cd4bccd7b623bfefa483a783b34
Konstantin Zangerle <info@konstantinzangerle.de>
2015-11-23 15:26:54 +0100

    Allow creation

--- create.php
+++ create.php
@@ -6,20 +6,24 @@ if( isset ($_POST["organ"]) && isset ($_POST["date"])){
   print("isset test");
   if (filter_var($_POST['date'], FILTER_VALIDATE_REGEXP,array("options"=>array("regexp"=>DATEREGEX)))) {
     $date = explode("/",$_POST['date']);
-    if (in_array($_POST['organ'],getOrgans())){ //sanitize _POST
+    if (! in_array($_POST['organ'],getOrgans())){ //sanitize _POST
+      die("Invalid Organ!");
+    }
+    $filename = $date[0] . "-" . $date[1] . "-" . $date[2] . ".md";
+    $filenamePub = REPORTDIR . SUBPUBLISHED . $_POST['organ'] . "/" . $date[0] . "-" . $date[1] . "-" . $date[2] . ".md";
 
-      $filename = $date[0] . "-" . $date[1] . "-" . $date[2] . ".md";
-      $filenamePub = REPORTDIR . SUBPUBLISHED . $_POST['organ'] . "/" . $date[0] . "-" . $date[1] . "-" . $date[2] . ".md";
-
-      if(is_file(REPORTDIR . SUBUNPUBLISHED . $_POST['organ'] . "/" . $filename)){ // locate to edit.php
-	header('Location: edit.php?file='.$filename . "&organ=" . $_POST['organ']);
-      }
-      if(is_file(REPORTDIR . SUBPUBLISHED . $_POST['organ'] . "/". $filename)){
-	die("Already published");
-      }
-      print($filename);
+    if(is_file(REPORTDIR . SUBUNPUBLISHED . $_POST['organ'] . "/" . $filename)){ // locate to edit.php
+      header('Location: edit.php?file='.$filename . "&organ=" . $_POST['organ']);
+      exit();
+    }
+    if(is_file(REPORTDIR . SUBPUBLISHED . $_POST['organ'] . "/". $filename)){
+      die("Already published");
     }
 
+    //  Copy Template file and open editor
+    copy(REPORTDIR . $_POST['organ'] . ".template.md", REPORTDIR . SUBUNPUBLISHED . $_POST['organ'] . "/" . $filename);
+    header('Location: edit.php?file='.$filename . "&organ=" . $_POST['organ']);
+    exit();
   }
 }
 
