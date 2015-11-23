2ea0af94c0d05f3f5d91e6cfb72ba2ddab5fa204
Konstantin Zangerle <info@konstantinzangerle.de>
2015-11-23 20:20:03 +0100

    Delete show.php

--- show.php
+++
@@ -1,7 +0,0 @@
-<?php
-require('defines.php');
-$cmd = "pandoc ". REPORTDIR . SUBPUBLISHED.  $_GET['organ'] . "/" . $_GET['file'] . " -f markdown -t html -s -o test.html";
-exec($cmd);
-
-header('Location: test.html');
-?>
