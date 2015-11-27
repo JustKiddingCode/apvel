5364502b99f0ad048c68b7664068c7da58a87bfe
Konstantin Zangerle <info@konstantinzangerle.de>
2015-11-27 14:44:37 +0100

    Refresh lock automatically

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+var timer;
+
+      function refreshWritePermission(file,organ){
+	getWritePermission(file,organ);
+	document.getElementById("publish").style.visibility = "hidden";
+	timer = setInterval(function() { getWritePermission(file,organ) },180000);
+      }
+
       function getWritePermission(file,organ) {
         var myAjax = new XMLHttpRequest();
 	myAjax.onreadystatechange = function() {
 	  if (myAjax.readyState == 4) {
 	    if (myAjax.status == 200) {
 	      Response = myAjax.responseXML.documentElement.firstChild.data;
-	      document.getElementById("locktext").innerHTML = Response;
+	      document.getElementById("locktext").innerHTML = Response + " will refresh automatically";
 	      document.getElementById("submitbutton").style.visibility = "visible";
 	    }
           }
