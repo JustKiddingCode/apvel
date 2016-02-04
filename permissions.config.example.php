e6c119cb495f94c84b5018896a36dbcf5a63a7be
Konstantin Zangerle <info@konstantinzangerle.de>
2016-02-04 23:21:00 +0100

    Change auth to have groups as array not as comma seperated

--- permissions.config.example.php
+++ permissions.config.example.php
@@ -1,53 +1,28 @@
 <?php
-$fskIntern = array("justkidding");
 
 $organs = array("fsk" => "Fachschaftenkonferenz",
 		"stupa" => "Studierenparlament",
 		"fsmi-fsr" => "FSMI Fachschaftsrat");
 
 
-$emailUN = array("fsk" => array("info@konstantinzangerle.de"));
-$emailPub = array("fsk" => array("info@konstantinzangerle.de"));
+$emailUN = array("fsk" => array("test@test.com"));
+$emailPub = array("fsk" => array("test@test.com"));
 
 function checkAdminPerms($organ) {
     if ($organ == "fsk") {
-      if (strpos($_SESSION['groups'],"fsk-praesidium") ||
-	  strpos($_SESSION['groups'], "admin")) {
+      if (in_array("fsk-praesidium", $_SESSION['groups'])) { 
 	    return true;
       }
-    } else if($organ == "stupa"){
-      if (strpos($_SESSION['groups'],"stupa-praesidium") ||
-	  strpos($_SESSION['groups'], "admin")) {
-	    return true;
-	}
-    }
+    }	    
     return false;
 }
 
-
+// you get the idea
 function checkWritePerms($organ) {
-    if ($organ == "fsk") {
-      if (strpos($_SESSION['groups'],$organ)) {
-        return true;
-      }
-    }  else if ($organ == "stupa") {
-      if (strpos($_SESSION['groups'],$organ)) {
-        return true;
-      }
-    }
 
     return false;
 }
 function checkReadPerms($organ) {
-    if ($organ == "fsk") {
-      if (strpos($_SESSION['groups'],$organ) || strpos($_SESSION['groups'],"stupa")) {
-        return true;
-      }
-    } else if ($organ == "stupa"){
-      if (strpos($_SESSION['groups'],$organ) || strpos($_SESSION['groups'],"fsk")) {
-        return true;
-      }
-    }
     return false;
 }
 
