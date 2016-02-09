d831bfe3965b58d095c1d1e8d47c368a16b1f156
Konstantin Zangerle <info@konstantinzangerle.de>
2016-02-09 19:16:59 +0100

    update sendmail auth system

--- sendmail.php
+++ sendmail.php
@@ -2,12 +2,8 @@
 require_once('lib.php');
 require_once('defines.php');
 
-$user = 'justkidding';
-
-
-
 if (isset($_GET['organ']) && isset($_GET['file'])) {
-  if (checkOrgan($_GET['organ']) && checkReadPerms($user, $_GET['organ']) && checkFilename($_GET['file'])) {
+  if (checkOrgan($_GET['organ']) && checkReadPerms($_GET['organ']) && checkFilename($_GET['file'])) {
     $to = REPORTDIR . SUBUNPUBLISHED . $_GET['organ'] . "/" . $_GET['file'];
     pandocToPDF($to, $to);
     echo writeEmail($_GET['organ'], $_GET['file'], SUBUNPUBLISHED, array($to. ".pdf"));
