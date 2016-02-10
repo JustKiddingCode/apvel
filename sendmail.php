7aaf56a054212902f823eb6fd38bf7d669110d96
Konstantin Zangerle <info@konstantinzangerle.de>
2016-02-10 15:51:35 +0100

    rewrite sendmail

--- sendmail.php
+++ sendmail.php
@@ -8,9 +8,11 @@ require_once('defines.php');
 
 if (isset($_GET['organ']) && isset($_GET['file'])) {
   if (checkOrgan($_GET['organ']) && checkReadPerms($_GET['organ']) && checkFilename($_GET['file'])) {
-    $to = REPORTDIR . SUBUNPUBLISHED . $_GET['organ'] . "/" . $_GET['file'];
+    $file = REPORTDIR . SUBUNPUBLISHED . $_GET['organ'] . "/" . $_GET['file'];
     pandocToPDF($to, $to);
-    echo writeEmail($_GET['organ'], $_GET['file'], SUBUNPUBLISHED, array($to. ".pdf"));
+    $text = file_get_contents(REPORTDIR . $_GET['organ']. ".email");
+    $text += file_get_contents($file);
+    echo rlyWriteEmail("justkidding@asta-kit.de","APVEL", $emailUN[$_GET['organ']], $text, array($to ".pdf", $to)); 
   }
 }
 
