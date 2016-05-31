f4929484533283dcc6b3e8023ce09dedfafeaef2
Konstantin Zangerle <info@konstantinzangerle.de>
2016-05-31 17:45:36 +0200

    allow dates to be 3X

--- defines.php
+++ defines.php
@@ -3,7 +3,7 @@ define('REPORTDIR', 'reports/');
 define('SUBUNPUBLISHED', 'unpublished/');
 define('SUBPUBLISHED', 'published/');
 
-define('DATEREGEX', ',[0-9][0-9][0-9][0-9]/[0-1][0-9]/[0-2][0-9],');
+define('DATEREGEX', ',[0-9][0-9][0-9][0-9]/[0-1][0-9]/[0-3][0-9],');
 define('INTERN_REGEX', ';\[intern\](.*?)\[/intern\];s');
 
 ?>
